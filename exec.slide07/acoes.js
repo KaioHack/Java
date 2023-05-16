4bcf31de913736cbb6c5d33d2995c5b42b32f27a
KaioHack <128508137+KaioHack@users.noreply.github.com>
2023-05-16 10:08:29 -0300

    Add files via upload

---
+++ exec.slide07/acoes.js
@@ -0,0 +1,20 @@
+/*Java do exercicio 07*/
+
+let res = window.document.getElementById('saida')
+
+function acao1(){
+    Response.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function acao2(){
+    Response.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+function acao3(){
+    Response.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function acao4(){
+    Response.innerHTML += '<p>Clicou no quarto botão</p>'
+}
+
